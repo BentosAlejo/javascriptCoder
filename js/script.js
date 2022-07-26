5f9b5016be9e64b9b4bc93345903c381ce435597
Alejo Bentos <bentosalejo@gmail.com>
2022-07-26 16:00:46 -0300

    agregando arrays/ desafio complementario

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 
-const producto1 = {nombre: "Puerta", precio: 15000, stock: 12}
-const producto2 = {nombre: "Ventana", precio: 9000, stock: 22}
-const producto3 = {nombre: "Inodoro", precio: 10000, stock: 15}
-const producto4 = {nombre: "Lavamanos", precio: 7000, stock: 16}
-let precioTotal = 0
+const productos = [
+     {nombre: "Puerta", precio: 15000, stock: 12},
+     {nombre: "Ventana", precio: 9000, stock: 22},
+     {nombre: "Inodoro", precio: 10000, stock: 15},
+     {nombre: "Lavamanos", precio: 7000, stock: 16}
+];
+    let precioTotal = 0
 
 let cantidadCompras = parseInt(prompt("Ingrese la cantidad de productos distintos que quiere comprar: \n-Puerta \n-Ventana \n-Inodoro \n-Lavamanos"))
 
@@ -11,14 +13,14 @@ for(let i = 0; i < cantidadCompras; i++ ){
     let compra1 = prompt("Ingrese el nombre del producto que quiere comprar: \n-Puerta \n-Ventana \n-Inodoro \n-Lavamanos")
     let cantidad1 = parseInt(prompt("Ingrese la cantidad del producto que quiere comprar: "))
     
-    if(compra1 = "Puerta"){
-        calculoGlobal(cantidad1, producto1.stock, producto1.precio)
-    }else if(compra1 = "Ventana"){
-        calculoGlobal(cantidad1, producto2.stock, producto2.precio)
-    }else if(compra1 = "Inodoro"){
-        calculoGlobal(cantidad1, producto3.stock, producto3.precio)
-    }else if(compra1 = "Lavamanos"){
-        calculoGlobal(cantidad1, producto4.stock, producto4.precio)
+    if(compra1 == "Puerta"){
+        calculoGlobal(compra1, cantidad1)
+    }else if(compra1 == "Ventana"){
+        calculoGlobal(compra1, cantidad1)
+    }else if(compra1 == "Inodoro"){
+        calculoGlobal(compra1, cantidad1)
+    }else if(compra1 == "Lavamanos"){
+        calculoGlobal(compra1, cantidad1)
     }else{
         alert("No tenemos ese producto")
     }
@@ -32,10 +34,13 @@ let datosEnvio = prompt("Ingrese Ciudad, direccion y horario de disponibilidad p
 let datosCliente = prompt("Ingrese su numero de telefono y su mail de contacto")
 alert("Muchas gracias por su compra,nos pondremos en contacto a la brevedad para concretar el pago")
 
-function calculoGlobal(cantidad, stock, precio){
-    if(stock >= cantidad){
-        calcularPrecio(cantidad, precio)
-        alert("EL precio es de: $" + (cantidad * precio))
+function calculoGlobal(producto, cantidad){
+
+    let productoEncontrado = productos.find(elemento => elemento.nombre === producto)
+    
+    if(productoEncontrado.stock >= cantidad){
+        calcularPrecio(cantidad, productoEncontrado.precio)
+        alert("El precio es de: $" + (cantidad * productoEncontrado.precio))
     }else{
         alert("Nuestro stock es de: " + stock + ".")
     }
